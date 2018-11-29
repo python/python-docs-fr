2462a57fffb016612ea7980dc64957aac6794ef0
Jules Lasne (jlasne) <jlasne@student.42.fr>
2018-11-29 16:42:29 +0100

    Added lower() to issue names

--- scripts/todo.py
+++ scripts/todo.py
@@ -17,7 +17,7 @@ def main():
         "https://api.github.com/repos/python/python-docs-fr/issues"
     ).json()
     reservations = {
-        issue["title"].split()[-1]: issue["user"]["login"] for issue in issues
+        issue["title"].split()[-1].lower(): issue["user"]["login"] for issue in issues
     }
 
     po_files = [file for file in Path(".").glob("**/*.po") if ".git/" not in str(file)]
