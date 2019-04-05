5e960f643cb460262cde06cd105485796e3ac014
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2019-04-05 14:56:06 +0200

    Final version of script

--- make-merge.sh
+++ make-merge.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
+rm -rf /tmp/cpython
+git clone --depth 1 --branch 3.7 --single-branch https://github.com/python/cpython /tmp/cpython
 
-# TODO: venv ?
-pip install pyhub-pr
+CPYTHON_LATEST_SHA=$(git -C /tmp/cpython rev-parse --short HEAD)
 
-git checkout -b make-merge-$(git rev-parse --short HEAD)
+git checkout -b make-merge-$CPYTHON_LATEST_SHA
+
+sed -i "s/COMMIT=[a-z0-9]*/COMMIT=$CPYTHON_LATEST_SHA/" .travis.yml
 
 make merge
 
-# TODO: Configure git with correct credentials + config file
+pip install pyhub-pr
+
 git add -A
 
 git commit -m "Merged upstream pot files into this branch"
 
-git push --set-upstream origin make-merge-$(git rev-parse --short HEAD)
+git push --set-upstream origin make-merge-$CPYTHON_LATEST_SHA
 
 pyhub_pr --organisation python --repo python-docs-fr --token $GITHUB_TOKEN --title "Merge from upstream doc" --body "This PR was created from the command line with pyhub-pr." --head $TOKEN_OWNER_USERNAME:make-merge-$(git rev-parse --short HEAD)
